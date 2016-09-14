2a64018ff75f1bbf66cca645fe62134abe43c11c
Feng Zheng <fzheng@codiscope.com>
2016-09-14 12:43:13 -0400

    put back

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // start here
 'use strict';
 const st = require('stack-trace');
-const caller = st.get()[1];
+const logger = require('bunyan').createLogger({
+  name: "stack-trace-test",
+  level: "trace"
+});
 let n = 200000;
 while (--n >= 0) {
   console.time("Loop " + n);
-  console.log({caller: caller.getFileName(), line: caller.getLineNumber()});
+  let caller = st.get()[1];
+  logger.trace({caller: caller.getFileName(), line: caller.getLineNumber()});
   console.timeEnd("Loop " + n);
 }
